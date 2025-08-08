cd28d138efea85d2deaaf564fae7f8d80fb767f0
dvale04 <84812619+dvale04@users.noreply.github.com>
2025-08-08 17:39:06 -0600

    added new photos

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,6 +3,17 @@ import Image2 from '../img/portfolio/2.png';
 import Image3 from '../img/portfolio/3.png';
 import Image4 from '../img/portfolio/4.png';
 import Image5 from '../img/portfolio/5.png';
+import Image6 from '../img/portfolio/6.png';
+import Image7 from '../img/portfolio/7.png';
+import Image8 from '../img/portfolio/8.png';
+import Image9 from '../img/portfolio/9.png';
+import Image10 from '../img/portfolio/10.png';
+import Image11 from '../img/portfolio/11.png';
+import Image12 from '../img/portfolio/12.png';
+import Image13 from '../img/portfolio/13.png';
+import Image14 from '../img/portfolio/14.png';
+import Image15 from '../img/portfolio/15.png';
+import Image16 from '../img/portfolio/16.png';
 // import link
 import { Link } from 'react-router-dom';
 // import motion
@@ -13,12 +24,21 @@ import { transition1 } from '../transitions';
 const Portfolio = () => {
   const portfolioImages = [
     { src: Image1, alt: 'A wide image', className: 'sm:col-span-2' },
+    {src: Image16, alt: 'A normal image', className: '' },
     { src: Image2, alt: 'A wide image', className: 'sm:col-span-2' },
     { src: Image3, alt: 'A normal image', className: '' },
     { src: Image4, alt: 'A tall image', className: 'sm:row-span-2' },
     { src: Image5, alt: 'A normal image', className: '' },
-    //{src: Image6, alt: 'A normal image', className: '' },
-    //{src: Image7, alt: 'A wide image', className: 'sm:col-span-2' },
+    {src: Image6, alt: 'A normal image', className: '' },
+    {src: Image7, alt: 'A wide image', className: 'sm:col-span-2' },
+    {src: Image8, alt: 'A normal image', className: '' },
+    {src: Image9, alt: 'A normal image', className: '' },
+    {src: Image10, alt: 'A normal image', className: '' },
+    {src: Image11, alt: 'A normal image', className: '' },
+    {src: Image12, alt: 'A normal image', className: '' },
+    {src: Image13, alt: 'A normal image', className: '' },
+    {src: Image14, alt: 'A normal image', className: '' },
+    {src: Image15, alt: 'A normal image', className: '' },
   ];
 
   return (
@@ -31,7 +51,7 @@ const Portfolio = () => {
   //   >
 
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 
-        gap-4'>
+        gap-8'>
         {portfolioImages.map((item, index) => (
           <div
             key={index}
