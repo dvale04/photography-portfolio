da86d4df3f7885bd7e1e6da77f09d7793e9c6621
dvale04 <84812619+dvale04@users.noreply.github.com>
2025-08-09 13:57:54 -0600

    pushed portfolio images down

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -51,7 +51,7 @@ const Portfolio = () => {
   //   >
 
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 
-        gap-8'>
+        gap-8 md:pt-[10px] pt-[20px]'>
         {portfolioImages.map((item, index) => (
           <div
             key={index}
